2bb8953c39437c80aa6472336273c68910568ef9
David Brázdil <david.brazdil@gmail.com>
2012-06-30 13:19:41 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamecaptcha::Application.config.secret_token = '942e4ff73a6a11ba4049d474efb94575444ff61cad3ac1b6444f65432bc9087ae2a21f2535a7fd90c864d1bbfda2046183b0eb81bd5638767f2feeef48ee8fff'
