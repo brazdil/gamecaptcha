456563982020e23de626b3fc8a5e2c17de32e459
David Brázdil <david.brazdil@gmail.com>
2012-07-01 09:52:00 +0100

    Proper way of touch on iPad

--- public/javascripts/captcha.js
+++ public/javascripts/captcha.js
@@ -3,36 +3,36 @@ function checkEnter(e){
  return (e.keyCode || event.which || event.charCode || 0) !== 13;
 }
 
-function touchHandler(event)
-{
- var touches = event.changedTouches,
-    first = touches[0],
-    type = "";
+// function touchHandler(event)
+// {
+//  var touches = event.changedTouches,
+//     first = touches[0],
+//     type = "";
 
-     switch(event.type)
-{
-    case "touchstart": type = "mousedown"; break;
-    case "touchmove":  type="mousemove"; break;        
-    case "touchend":   type="mouseup"; break;
-    default: return;
-}
-var simulatedEvent = document.createEvent("MouseEvent");
-simulatedEvent.initMouseEvent(type, true, true, window, 1,
-                          first.screenX, first.screenY,
-                          first.clientX, first.clientY, false,
-                          false, false, false, 0/*left*/, null);
+//      switch(event.type)
+// {
+//     case "touchstart": type = "mousedown"; break;
+//     case "touchmove":  type="mousemove"; break;        
+//     case "touchend":   type="mouseup"; break;
+//     default: return;
+// }
+// var simulatedEvent = document.createEvent("MouseEvent");
+// simulatedEvent.initMouseEvent(type, true, true, window, 1,
+//                           first.screenX, first.screenY,
+//                           first.clientX, first.clientY, false,
+//                           false, false, false, 0/*left*/, null);
 
-first.target.dispatchEvent(simulatedEvent);
-event.preventDefault();
-}
+// first.target.dispatchEvent(simulatedEvent);
+// event.preventDefault();
+// }
 
-function init()
-{
-   document.addEventListener("touchstart", touchHandler, true);
-   document.addEventListener("touchmove", touchHandler, true);
-   document.addEventListener("touchend", touchHandler, true);
-   document.addEventListener("touchcancel", touchHandler, true);    
-}
+// function init()
+// {
+//    document.addEventListener("touchstart", touchHandler, true);
+//    document.addEventListener("touchmove", touchHandler, true);
+//    document.addEventListener("touchend", touchHandler, true);
+//    document.addEventListener("touchcancel", touchHandler, true);    
+// }
 
 function start() {
 	var goldTop = 10;
@@ -152,8 +152,10 @@ $(function() {
 	$("#cokeCan_Silver").hide();
 	$("#cokeCan_Bronze").hide();
 
+	(new Image()).src = 'images/ColaPodium.png';
+
 	$("#cokeRun_CAPTCHA").click(function() {
-		init();
+		// init();
 		$("#cokeRun_CAPTCHA").unbind('click');
 		$("#cokeCan_Red").show();
 		$("#cokeCan_Gold").show();
